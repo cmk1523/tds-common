18927f51af68cd2ef8ea7b981e75447820133c83
Christopher Kolodziejczyk <cmk1523@gmail.com>
2019-12-04 18:12:50 -0800

    update

--- src/main/java/com/techdevsolutions/common/beans/auditable/Auditable.java
+++ src/main/java/com/techdevsolutions/common/beans/auditable/Auditable.java
@@ -1,9 +1,13 @@
 package com.techdevsolutions.common.beans.auditable;
 
+import com.techdevsolutions.common.service.core.DateUtils;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 public class Auditable implements Serializable, Comparable<Auditable> {
@@ -16,12 +20,25 @@ public class Auditable implements Serializable, Comparable<Auditable> {
     @NotNull
     private Long created;
 
+    @NotNull
+    private String createdStr;
+
+    @NotNull
+    private List<@NotBlank String> tags = new ArrayList<>();
+
+    @Override
+    public int compareTo(Auditable o) {
+        return this.getId().compareTo(o.getId());
+    }
+
     @Override
     public String toString() {
         return "Auditable{" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 ", created=" + created +
+                ", createdStr='" + createdStr + '\'' +
+                ", tags=" + tags +
                 '}';
     }
 
@@ -32,19 +49,33 @@ public class Auditable implements Serializable, Comparable<Auditable> {
         Auditable auditable = (Auditable) o;
         return Objects.equals(id, auditable.id) &&
                 Objects.equals(name, auditable.name) &&
-                Objects.equals(created, auditable.created);
+                Objects.equals(created, auditable.created) &&
+                Objects.equals(createdStr, auditable.createdStr) &&
+                Objects.equals(tags, auditable.tags);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(id, name, created);
+        return Objects.hash(id, name, created, createdStr, tags);
     }
 
-    @Override
-    public int compareTo(Auditable o) {
-        return this.getId().compareTo(o.getId());
+    public String getCreatedStr() {
+        return createdStr;
     }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public Auditable setTags(List<String> tags) {
+        this.tags = tags;
+        return this;
+    }
+//    public Auditable setCreatedStr(String createdStr) {
+//        this.createdStr = createdStr;
+//        return this;
+//    }
 
     public String getId() {
         return id;
@@ -70,6 +101,11 @@ public class Auditable implements Serializable, Comparable<Auditable> {
 
     public Auditable setCreated(Long created) {
         this.created = created;
+
+        if (this.created != null) {
+            this.createdStr = DateUtils.DateToISO(this.created);
+        }
+
         return this;
     }
 }
--- src/main/java/com/techdevsolutions/common/dao/elasticsearch/events/EventElasticsearchDAO.java
+++ src/main/java/com/techdevsolutions/common/dao/elasticsearch/events/EventElasticsearchDAO.java
@@ -6,17 +6,41 @@ import com.techdevsolutions.common.beans.elasticsearchCommonSchema.Event;
 import com.techdevsolutions.common.dao.elasticsearch.BaseElasticsearchHighLevel;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.PredicateUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.ElasticsearchStatusException;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.*;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchModule;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class EventElasticsearchDAO extends BaseElasticsearchHighLevel {
+    public static final String INDEX_BASE_NAME = "events";
+
+    public static final String ACTION_CREATED = "created";
+    public static final String CODE_CREATED = "201";
+    public static final String KIND_CREATE = "create";
+
+    public static final String ACTION_REMOVED = "removed";
+    public static final String CODE_REMOVED = "204";
+    public static final String KIND_REMOVED = "remove";
+
+    public static final String ACTION_UPDATED = "updated";
+    public static final String CODE_UPDATED = "200";
+    public static final String KIND_UPDATED = "update";
+
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     public final EventRowMapper rowMapper = new EventRowMapper();
     private String index = "events";
@@ -102,5 +126,175 @@ public class EventElasticsearchDAO extends BaseElasticsearchHighLevel {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public Event getByEventId(final String documentId, final String index) throws Exception {
+        if (StringUtils.isEmpty(documentId)) {
+            throw new IllegalArgumentException("documentId is null or empty");
+        }
+
+        GetResponse getResponse = this.getDocument(documentId, index);
+        return this.rowMapper.fromJson(getResponse.getSourceAsString());
+    }
+
+    public List<Event> getEventsByEventDataId(final String id, final String category, final String dataset) throws Exception {
+        if (StringUtils.isEmpty(id)) {
+            throw new IllegalArgumentException("id is null or empty");
+        }
+
+        BaseElasticsearchHighLevel baseElasticsearchHighLevel = new BaseElasticsearchHighLevel();
+        baseElasticsearchHighLevel.getClient(this.getHost());
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        SearchModule searchModule = new SearchModule(Settings.EMPTY, false, Collections.emptyList());
+
+        String query = "{\n" +
+                "  \"size\": 10000,\n" +
+                "  \"sort\": [\n" +
+                "    {\n" +
+                "      \"event.created\": {\n" +
+                "        \"order\": \"desc\"\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"query\": {\n" +
+                "    \"bool\": {\n" +
+                "      \"filter\": [\n" +
+                "        {\n" +
+                "          \"term\": {\n" +
+                "            \"event.data.id.keyword\": \"" + id + "\"\n" +
+                "          }\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"term\": {\n" +
+                "            \"event.category\": \"" + category + "\"\n" +
+                "          }\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"term\": {\n" +
+                "            \"event.dataset\": \"" + dataset + "\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+
+        try {
+            NamedXContentRegistry namedXContentRegistry = new NamedXContentRegistry(searchModule.getNamedXContents());
+            XContent xContent = XContentFactory.xContent(XContentType.JSON);
+            XContentParser parser = xContent.createParser(namedXContentRegistry,
+                    DeprecationHandler.THROW_UNSUPPORTED_OPERATION, query);
+            searchSourceBuilder.parseXContent(parser);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        SearchRequest searchRequest = new SearchRequest(this.getIndex());
+        searchRequest.source(searchSourceBuilder);
+        searchRequest.scroll(TimeValue.timeValueMinutes(1L));
+
+        List<SearchHit> results = this.getDocumentsWithScroll(searchRequest);
+        return results.stream().map((i) -> {
+            try {
+                Event event = this.rowMapper.fromJson(i.getSourceAsString());
+                event.setId(i.getId());
+                return event;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }).filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
+    public Event getEventByEventDataId(final String id, final String category, final String dataset) throws Exception {
+        if (StringUtils.isEmpty(id)) {
+            throw new IllegalArgumentException("id is null or empty");
+        }
+
+        BaseElasticsearchHighLevel baseElasticsearchHighLevel = new BaseElasticsearchHighLevel();
+        baseElasticsearchHighLevel.getClient(this.getHost());
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        SearchModule searchModule = new SearchModule(Settings.EMPTY, false, Collections.emptyList());
+
+        String query = "{\n" +
+                "  \"size\": 1,\n" +
+                "  \"sort\": [\n" +
+                "    {\n" +
+                "      \"event.created\": {\n" +
+                "        \"order\": \"desc\"\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"query\": {\n" +
+                "    \"bool\": {\n" +
+                "      \"filter\": [\n" +
+                "        {\n" +
+                "          \"term\": {\n" +
+                "            \"event.data.id.keyword\": \"" + id + "\"\n" +
+                "          }\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"term\": {\n" +
+                "            \"event.category\": \"" + category + "\"\n" +
+                "          }\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"term\": {\n" +
+                "            \"event.dataset\": \"" + dataset + "\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+
+        try {
+            NamedXContentRegistry namedXContentRegistry = new NamedXContentRegistry(searchModule.getNamedXContents());
+            XContent xContent = XContentFactory.xContent(XContentType.JSON);
+            XContentParser parser = xContent.createParser(namedXContentRegistry,
+                    DeprecationHandler.THROW_UNSUPPORTED_OPERATION, query);
+            searchSourceBuilder.parseXContent(parser);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        SearchRequest searchRequest = new SearchRequest(this.getIndex());
+        searchRequest.source(searchSourceBuilder);
+        searchRequest.scroll(TimeValue.timeValueMinutes(1L));
+
+        List<SearchHit> results = this.getDocumentsWithScroll(searchRequest);
+
+        if (results.size() == 0) {
+            throw new Exception("Unable to find item by id: " + id);
+        }
+
+        SearchHit searchHit = results.get(0);
+        Event event = this.rowMapper.fromJson(searchHit.getSourceAsString());
+        event.setId(searchHit.getId());
+        return event;
+    }
+
+    public Event getEventByEventDataIdLazy(final String id, final String category, final String dataset) throws Exception {
+        int currentRetry = 0;
+
+        while(currentRetry < 10 * 2) {
+            try {
+                Event event = this.getEventByEventDataId(id, category, dataset);
+//                this.logger.info("Took " + currentRetry + " retries");
+                return event;
+            } catch (Exception e) {
+                if (!e.getMessage().contains("Unable to find item by id: ")) {
+                    throw e;
+                }
+
+                currentRetry++;
+                Thread.sleep(50L);
+            }
+        }
+
+        throw new Exception("Unable to find item by id: " + id);
     }
 }
